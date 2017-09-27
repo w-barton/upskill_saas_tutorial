11170071d661bd4c6adb79fc20af7caf9e4baa37
Will Barton <will.barton@live.com>
2017-09-27 19:48:33 +0000

    Added mailer settings to environment file 2

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
     :port            =>  ENV['MAILGUN_SMTP_PORT'],
     :address         =>  ENV['MAILGUN_SMTP_SERVER'],
     :user_name       =>  ENV['MAILGUN_SMTP_LOGIN'],
-    :password        =>  ENV['MAILGUN_SMTP_PORT'],
+    :password        =>  ENV['MAILGUN_SMTP_PASSWORD'],
     :domain          =>  'dry-inlet-75578.herokuapp.com',
     :authentication  =>  :plain,
 }
