cb12bead99e2685ace57a93cfa9aeeba4ab42365
Will Barton <will.barton@live.com>
2017-11-06 05:06:30 +0000

    Trying things to fix my 'Plan must exist' error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   attr_accessor :stripe_card_token
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
